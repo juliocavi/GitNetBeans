6c820104f8a606debc52ead8cd157f82189b5e8d
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-06-07 22:49:26 +0200

    Commit 1 para proyecto GitNetBeans

---
+++ src/gitnetbeans/GitNetBeans.java
@@ -0,0 +1,11 @@
+package gitnetbeans;
+
+public class GitNetBeans {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Ejemplo de Git con NetBeans");
+        
+    }
+    
+}
